e0e2d6031d1cb1a1f5eb67e1d5dcaf4d779c6d35
MaciejBuczek <maciejbuczek98@gmail.com>
2019-12-15 18:57:40 +0100

    basic script for interactable objects

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 1.5f;
+
+    private bool isFocused = false;
+    private Vector3 playerPosition;
+
+    public virtual void Interact()
+    {
+        Debug.Log("interacted with " + transform.name);
+    }
+    private void OnMouseDown()
+    {
+        Debug.Log("clicked on " + transform.name);
+        isFocused = true;
+    }
+    private void Start()
+    {
+        //playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+    }
+    private void Update()
+    {
+        if (isFocused)
+        {
+            playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+            if (Vector3.Distance(transform.position, playerPosition) <= radius)
+            {
+                Interact();
+                isFocused = false;
+            }
+        }
+    }
+}
